2abe2b69097dacf7fb341a20d6f64b27d10ab4e0
Fernando Arruda <fernandoarrudalucas@gmail.com>
2025-10-22 14:42:43 -0300

    Versão inicial do projeto Ótima Consultoria

---
+++ src/test/java/com/otima/consultoria/ConsultoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otima.consultoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
